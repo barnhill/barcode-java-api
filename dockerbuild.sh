a3abe07dc1bed86d6af59095a125536037899558
Brad Barnhill <brad.barnhill@cbsinteractive.com>
2019-01-03 20:31:30 -0600

    switch to docker-compose, create linux and windows yml files for docker-compose to build different services, update build script

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -1,3 +1,5 @@
 ./gradlew clean bootJar
-docker build -t bradbarnhill/barcode-api:latest .
-docker push bradbarnhill/barcode-api
+#docker build -t bradbarnhill/barcode-api:latest .
+docker-compose build linux
+docker-compose push linux
+#docker push bradbarnhill/barcode-api
