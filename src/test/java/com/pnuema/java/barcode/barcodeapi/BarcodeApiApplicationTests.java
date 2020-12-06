5512e8dc8e05216d80cc5ee17335ff4503e8cac0
Brad Barnhill <brad.barnhill@procore.com>
2020-12-05 21:06:38 -0600

    Update to Spring 2.4.0

--- src/test/java/com/pnuema/java/barcode/barcodeapi/BarcodeApiApplicationTests.java
+++ src/test/java/com/pnuema/java/barcode/barcodeapi/BarcodeApiApplicationTests.java
@@ -1,16 +1,11 @@
 package com.pnuema.java.barcode.barcodeapi;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 public class BarcodeApiApplicationTests {
-
     @Test
     public void contextLoads() {
     }
-
 }
