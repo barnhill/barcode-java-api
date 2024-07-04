b2a110c0a10c6c9d8ae07b825c5aa4be3b5fe213
Brad Barnhill <bradfordbarnhill@gmail.com>
2024-07-04 18:47:44 -0500

    fix v1 resource mapping

--- src/main/java/com/pnuema/java/barcode/barcodeapi/controllers/v1/AbstractV1Resource.java
+++ src/main/java/com/pnuema/java/barcode/barcodeapi/controllers/v1/AbstractV1Resource.java
@@ -4,7 +4,7 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping(
-        name = "/v1",
+        path = "/v1",
         produces = { MediaType.IMAGE_PNG_VALUE, MediaType.IMAGE_GIF_VALUE }
 )
 public abstract class AbstractV1Resource {}
